16d964d8d248f7d7025fef7b010f52f09180e59a
Shyam2k6 <153609679+Shyam2k6@users.noreply.github.com>
2024-12-20 21:19:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,17 +24,21 @@ const dataRender = async function () {
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`;
     const response = await fetch(url);
     const [data] = await response.json();
-    console.log(data);
     const { partOfSpeech } = data.meanings[0];
     const { phonetic } = data;
     const definitions = data.meanings[0].definitions[0].definition;
 
-    searchedWord.textContent = data.word;
-    pos.textContent = partOfSpeech;
-    phonetics.textContent = phonetic;
-    definition.textContent = definitions;
+    box.style.textAlign = "left";
+    const element = `<div class="heading">
+          <h2 class="searchedWord">${data.word}</h2>
+          <div class="pos">${partOfSpeech}</div>
+        </div>
+        <p class="phonetics">${phonetic}</p>
+        <p class="definition">${definitions}</p>`;
+    box.innerHTML = element;
   } catch (e) {
     const html = `<h1>No Results Found for ${word.value}</h1>`;
     box.innerHTML = html;
+    box.style.textAlign = "center";
   }
 };
